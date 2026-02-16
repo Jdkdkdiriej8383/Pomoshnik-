7b99b3a8a26270e57e6d45bbec6023271ef5691c
Jdkdkdiriej8383 <denrudik2011@vk.com>
2026-02-16 21:15:22 +0500

    Установка основного кл. рук

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 BOT_TOKEN = "8358402394:AAFsp3-PPZB5-HixEWLm8wXlARHNszxj7LA"           # ← замените
-TEACHER_ID = 1965081517             # ← ваш ID
+TEACHER_ID = 1407739698            # ← ваш ID
 TEACHER_TIMEZONE_OFFSET = 5            # ← ваш UTC
 CHANNEL_ID = "@testi_bjtjv"     # ← канал
